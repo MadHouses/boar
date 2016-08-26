9aeeb72bd8837cea70255e20cea37c54806e0b86
pigo <rushingpig@163.com>
2016-08-27 00:20:25 +0800

    init the project

---
+++ README.md
@@ -0,0 +1,7 @@
+## Prerequired
+
+* node 6.x
+
+## Contributed
+
+rushingpig@163.com
---
+++ index.js
@@ -0,0 +1,6 @@
+/**
+ * @Author : pigo
+ * @Email : rushingpig@163.com
+ * @Date : 16/8/26 23:06
+ */
+'use strict';
